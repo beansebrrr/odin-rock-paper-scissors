10d9c70e62be0c0e49d0033d4e3bd5404649b101
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-14 23:11:46 +0400

    ✨ oops i already finished it

---
+++ script.js
@@ -0,0 +1,61 @@
+/**
+ * Pock Scaper Rissors
+*/
+
+const weapons = ["rock", "paper", "scissors"];
+
+const conditions = {
+  "rock" :      {"paper" : false, "scissors" : true},
+  "paper" :     {"scissors" : false, "rock" : true},
+  "scissors" :  {"rock" : false, "paper" : true},
+};
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+function playRound(player, comp) {
+  if (comp == player) {
+    console.log("Tie");
+  } else if (isWinner(player, comp)) {
+    console.log("Win");
+    playerScore++;
+  } else if (isWinner(comp, player)) {
+    console.log("Lose");
+    computerScore++;
+  } else {
+    console.log("Something really bad happened.");
+  };
+};
+
+function playGame(rounds) {
+  for (let i = 0; i < rounds; i++) {
+    playRound(playerShoot(), computerShoot());
+    console.log(`Player: ${playerScore}\nComputer: ${computerScore}`);
+  };
+};
+
+function computerShoot() {
+  const i = Math.floor(Math.random() * 3);
+  console.log(`Computer shoots ${weapons[i]}`);
+  return weapons[i];
+};
+
+function playerShoot() {
+  let p = '';
+  function isAWeapon(input) {
+    return (weapons.indexOf(input.toLowerCase()) > -1);
+  };
+  while (!isAWeapon(p)) {
+    p = prompt("Rock, paper, or scissors?");
+  };
+  console.log(`Player shoots ${p}`);
+  return p;
+}
+
+function isWinner(main, opponent) {
+  return conditions[main][opponent];
+};
+
+
+playGame(5);
