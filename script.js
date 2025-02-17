cd9088b9b13da2d6bd7bfc86bb94d2f8a877bd74
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-17 22:49:57 +0400

    🎨 UI is functional

--- script.js
+++ script.js
@@ -3,9 +3,7 @@
 */
 
 const btnSection  = document.querySelector("div.btnSection")
-
-btnSection.addEventListener("click", (e) => playerShoot(e))
-
+btnSection.addEventListener("click", (e) => playRound(e))
 
 const conditions = {
   "rock" : {
@@ -39,58 +37,100 @@ const conditions = {
     "lizard" : false,
   },
 };
+const emojis = {
+  "rock" : "✊",
+  "paper" : "🖐️",
+  "scissors" : "✌️",
+  "lizard" : "🫳",
+  "spock" : "🖖",
+}
+
 const weapons = Object.keys(conditions)
 let playerScore = 0;
 let computerScore = 0;
 
 const playerActionText = document.querySelector(".playerAction")
+const playerScoreText = document.querySelector(".playerScore")
 const computerActionText = document.querySelector(".computerAction")
+const computerScoreText = document.querySelector(".computerScore")
+
+const playerChoiceBox = document.querySelector("#playerChoice")
+const computerChoiceBox = document.querySelector("#computerChoice")
 const resultText = document.querySelector("#resultText")
 
-function playRound(player, comp) {
-  playerActionText.textContent = `You used ${player}!`
-  computerActionText.textContent = `Computer used ${comp}!`
-  
+const modalContainer = document.querySelector(".modalContainer")
+const modalHeader = document.querySelector(".modal>h1");
+const restartButton = document.querySelector(".modal>button");
+
+function playRound(e) {
+  const player = playerShoot(e)
+  const comp = computerShoot()
 
   resultText.classList = []
+
   if (comp == player) {
     resultText.textContent = "Tie!"
-  }
-  else if (isWinner(player, comp)) {
-    resultText.textContent = "You win!"
-    resultText.classList.add("victory")
+  } else if (determineWinner(player, comp)) {
+    resultText.classList.add("victory");
+    resultText.textContent = `${player} wins against ${comp}!`;
     playerScore++;
-  }
-  else if (isWinner(comp, player)) {
-    resultText.textContent = "You lost!"
-    resultText.classList.add("loss")
+  } else if (!determineWinner(player, comp)) {
+    resultText.classList.add("loss");
+    resultText.textContent = `${player} loses to ${comp}.`
     computerScore++;
-  }
-  else {
+  } else {
     console.log("Something really bad happened.");
   };
+  updateScreen()
+};
+
+function updateScreen() {
+  playerScoreText.textContent = playerScore;
+  computerScoreText.textContent = computerScore;
+  if (playerScore > 4) { popUp("You won!") }
+  else if (computerScore > 4) { popUp("You lost!") };
 };
 
 
 function computerShoot() {
   const i = Math.floor(Math.random() * weapons.length);
-  console.log(`Computer shoots ${weapons[i]}`);
-  return weapons[i];
+  const choice = weapons[i]
+  console.log(`Computer shoots ${choice}`);
+  computerChoiceBox.textContent = emojis[choice]
+  return choice;
 };
 
 function playerShoot(e) {
   let choice = e.target.id;
   
-  function isAWeapon(input) {
-    return (weapons.indexOf(input) > -1);
-  };
+  function isAWeapon(input) { return (weapons.indexOf(input) > -1) };
+  
   if (isAWeapon(choice)) {
     console.log(`Player shoots ${choice}`)
-    playRound(choice, computerShoot());
+    playerChoiceBox.textContent = emojis[choice]
+    return (choice);
   };
-}
+};
 
-
-function isWinner(main, opponent) {
+function determineWinner(main, opponent) {
   return conditions[main][opponent];
+};
+
+
+function restartGame() {
+  playerChoiceBox.textContent = ''
+  computerChoiceBox.textContent = ''
+  resultText.textContent = '';
+  modalContainer.style.display = "none"
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreText.textContent = playerScore;
+  computerScoreText.textContent = computerScore;
+} 
+
+function popUp(message) {
+  modalHeader.textContent = message;
+  modalContainer.style.display = "flex"
+
+  restartButton.addEventListener("click", () => restartGame());
 };
